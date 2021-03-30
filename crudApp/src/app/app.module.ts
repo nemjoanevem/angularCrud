8903310a1b23a69c13aa9f51ec19af04e8685dad
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-30 13:27:54 +0200

    nav routing

--- crudApp/src/app/app.module.ts
+++ crudApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,13 @@ import { UpdateComponent } from './update/update.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "list", component: ListComponent},
+      {path: "create", component: CreateComponent},
+      {path: "update", component: UpdateComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
