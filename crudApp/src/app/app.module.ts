8c96b6bed07a3a15988f0a67ade567b347b715bf
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-30 13:06:41 +0200

    create create component

--- crudApp/src/app/app.module.ts
+++ crudApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
