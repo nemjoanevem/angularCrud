d2922739b257ed6d8359b23b4f17592acacce4d4
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-30 19:00:48 +0200

    loading every character from api

--- crudApp/src/app/app.component.ts
+++ crudApp/src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudApp';
-  url = 'https://finalspaceapi.com/api/v0/character?limit=6';
+  url = 'https://finalspaceapi.com/api/v0/character';
   constructor(private http: HttpClient) {
     this.http.get(this.url).toPromise().then((data: any) => {
-      console.log(`>>> data => `, data);
+      
    
       for (const key in data){
         if (data.hasOwnProperty(key)){
