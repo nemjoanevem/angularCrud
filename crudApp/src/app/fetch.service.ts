2e6c0890c3f846aa316c0ec904d9cada1237879b
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-30 17:34:02 +0200

    removing fetch service

--- crudApp/src/app/fetch.service.ts
+++ crudApp/src/app/fetch.service.ts
@@ -1,4 +1,4 @@
-import { Character } from './dataObj';
+/*import { Character } from './dataObj';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -10,8 +10,11 @@ export class FetchService {
 
   characterUrl = 'https://finalspaceapi.com/api/v0/character';
   public characters: Character[] = [];
+ 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    
+  }
 
   public fetchCharacters(): Observable<any> {
     return this.http.get(this.characterUrl);
@@ -20,3 +23,4 @@ export class FetchService {
 }
 
 
+*/
