397a34b989bd7ec5d8788ff84b26c2abb5224d44
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-30 14:52:34 +0200

    creating data object

---
+++ crudApp/src/app/dataObj.ts
@@ -0,0 +1,12 @@
+export class Character{
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    gender: string;
+    hair: string;
+    alias: string;
+    origin: string;
+    abalities: string;
+    img_url: string;
+}
