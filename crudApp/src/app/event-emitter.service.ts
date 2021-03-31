e991046fa2458f05c2af71b658d211bc5d942572
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-31 16:54:58 +0200

    removing event-emitter-service

---
+++ crudApp/src/app/event-emitter.service.ts
@@ -0,0 +1,19 @@
+/*import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventEmitterService {
+
+  invokeUpdateComponentFunction = new EventEmitter();
+  subsVar: Subscription; 
+
+  constructor() { }
+
+  
+  onUpdateComponentButtonClick(){
+    this.invokeUpdateComponentFunction.emit("s");
+  }
+}
+*/
