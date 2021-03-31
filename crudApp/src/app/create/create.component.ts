00ed2f8b0e26e025515c737a02d347090f3990fb
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-03-31 16:55:32 +0200

    creating create function

--- crudApp/src/app/create/create.component.ts
+++ crudApp/src/app/create/create.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
+  newCharacter : Character = new Character;
+
   constructor() { }
+
+  createNewCharacter(){
+    let count : any = 0;
+    let fromStorage : any = JSON.parse(localStorage.getItem('data') || '{}') as Character;
+    for(const key in fromStorage){
+      if (fromStorage.hasOwnProperty(key)){
+        count++;
+        }
+    }
+    
+    this.newCharacter.id === count+1;
+    this.newCharacter.name = (<HTMLInputElement>document.getElementById('name')).value;
+    this.newCharacter.origin = (<HTMLInputElement>document.getElementById('origin')).value;
+    this.newCharacter.gender = (<HTMLInputElement>document.getElementById('gender')).value;
+    this.newCharacter.status = (<HTMLInputElement>document.getElementById('status')).value;
+    this.newCharacter.img_url = "https://finalspaceapi.com/api/character/avatar/hue.jpg";
+
+    localStorage.setItem("newCharacter", JSON.stringify(this.newCharacter));
+  }
 
   ngOnInit(): void {
   }
