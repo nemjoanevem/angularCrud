21917ab55314b1578ae9c9bea956f1c7a5e31fdb
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-04-01 13:32:28 +0200

    updating login page

--- crudApp/src/app/login/login.component.ts
+++ crudApp/src/app/login/login.component.ts
@@ -11,19 +11,38 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   user : user = new user;
+  loginCheck : user = new user;
+  loginMessage : any = "Login required";
 
   ngOnInit(): void {
+    
+    this.loginStatus();
   }
 
   login(){
-    
-    if(this.user.name && this.user.password && this.user.name.length > 2 && this.user.password.length > 2){
-      this.user.name = (<HTMLInputElement>document.getElementById('login')).value;
-      this.user.password = (<HTMLInputElement>document.getElementById('password')).value;
+    this.user.name = (<HTMLInputElement>document.getElementById('login')).value;
+    this.user.password = (<HTMLInputElement>document.getElementById('password')).value;
+
+    if(this.user.name != null && this.user.password != null && this.user.name.length > 2 && this.user.password.length > 2){
+      
+      localStorage.setItem("currentUser", JSON.stringify(this.user));
+
     }
     else{
       alert("Túl rövid a felhasználó név vagy a jelszó! \nMinimum 3 karakterből kell állniuk!");
     }
+    this.loginStatus();
+
   
+  }
+
+  loginStatus(){
+    this.loginCheck = JSON.parse(localStorage.getItem('currentUser') || '{}') as user;
+    if(this.loginCheck.name === "admin" && this.loginCheck.password === "admin"){
+      this.loginMessage = "Logged in as admin";
+    }
+    else{
+      this.loginMessage = "Login required";
+  }
   }
 }
