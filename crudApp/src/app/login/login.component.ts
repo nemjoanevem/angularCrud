fcec14719272f9efef606b8070e8d0e1716ea1ee
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-04-01 10:53:58 +0200

    updating login page

--- crudApp/src/app/login/login.component.ts
+++ crudApp/src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { user } from './../dataObj';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +10,20 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  user : user = new user;
+
   ngOnInit(): void {
   }
 
+  login(){
+    
+    if(this.user.name && this.user.password && this.user.name.length > 2 && this.user.password.length > 2){
+      this.user.name = (<HTMLInputElement>document.getElementById('login')).value;
+      this.user.password = (<HTMLInputElement>document.getElementById('password')).value;
+    }
+    else{
+      alert("Túl rövid a felhasználó név vagy a jelszó! \nMinimum 3 karakterből kell állniuk!");
+    }
+  
+  }
 }
